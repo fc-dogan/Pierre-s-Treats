47ec24cf8f4d93892f28f244ba43f5966a03da30
Fatma <fceyda@gmail.com>
2020-03-27 10:52:59 -0700

    create delete and Edit cshtml files for Flavors and edit FlavorsController

--- PierresTreats/Controllers/FlavorsController.cs
+++ PierresTreats/Controllers/FlavorsController.cs
@@ -43,33 +43,33 @@ namespace PierresTreats.Controllers
       return View(thisFlavor);
     }
 
-    // public ActionResult Edit(int id)
-    // {
-    //   var thisFlavor = _db.Flavors.FirstOrDefault(Flavor => Flavor.FlavorId == id);
-    //   return View(thisFlavor);
-    // }
+    public ActionResult Edit(int id)
+    {
+      var thisFlavor = _db.Flavors.FirstOrDefault(Flavor => Flavor.FlavorId == id);
+      return View(thisFlavor);
+    }
 
-    // [HttpPost]
-    // public ActionResult Edit(Flavor Flavor)
-    // {
-    //   _db.Entry(Flavor).State = EntityState.Modified;
-    //   _db.SaveChanges();
-    //   return RedirectToAction("Index");
-    // }
+    [HttpPost]
+    public ActionResult Edit(Flavor Flavor)
+    {
+      _db.Entry(Flavor).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
 
-    // public ActionResult Delete(int id)
-    // {
-    //   var thisFlavor = _db.Flavors.FirstOrDefault(Flavor => Flavor.FlavorId == id);
-    //   return View(thisFlavor);
-    // }
+    public ActionResult Delete(int id)
+    {
+      var thisFlavor = _db.Flavors.FirstOrDefault(Flavor => Flavor.FlavorId == id);
+      return View(thisFlavor);
+    }
 
-    // [HttpPost, ActionName("Delete")]
-    // public ActionResult DeleteConfirmed(int id)
-    // {
-    //   var thisFlavor = _db.Flavors.FirstOrDefault(Flavor => Flavor.FlavorId == id);
-    //   _db.Flavors.Remove(thisFlavor);
-    //   _db.SaveChanges();
-    //   return RedirectToAction("Index");
-    // }
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      var thisFlavor = _db.Flavors.FirstOrDefault(Flavor => Flavor.FlavorId == id);
+      _db.Flavors.Remove(thisFlavor);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
